431424d4f589b75822b82ce64d9b394204cfdd10
Chris Ampenberger <christian@ampenberger.com>
2020-11-23 15:56:27 -0500

    Added login page

--- src/start.js
+++ src/start.js
@@ -6,9 +6,13 @@ import { jsonGet } from './httpUtil'
 
 import express from 'express'
 import nunjucks from 'nunjucks'
+import cookieSession from 'cookie-session'
+import querystring from 'querystring'
 
 const port = 5000
-const health_system_id = 4707
+const healthSystemId = 4707
+const loginUrl = '/login'
+const appPassword = 'Geheim'
 
 // Fetches the patient record and the $everything queri for a
 // patient by patient_id and returns an all promise with the results
@@ -55,9 +59,33 @@ function fetchPatientList(access_token) {
     })
 }
 
+// Filter that checks whether the user has given a valid access code.
+// If not, the user will we redirected to the target page.
+function loginFilter(redirectUrl) {
+    return (req, res, next) => {
+        if (req.session.userIsLoggedIn) {
+            logger.debug('loginFilter: user is logged in.')
+            // increment, so the session is refreshed
+            req.session.count = (req.session.count || 0) + 1
+            next()
+        } else {
+            const target = redirectUrl + '?' + querystring.stringify({ target: req.originalUrl, error: null })
+            logger.debug('loginFilter: user is not logged in. Redirecting to %s', target)
+            res.redirect(302, target)
+        }
+    }
+}
+
 // set up the express app,
 const app = express()
     .use(accessLogFilter)
+    .use(express.urlencoded({ extended: true }))
+    .use(cookieSession({
+        name: '1up',
+        // 1 hour
+        maxAge: 1 * 60 * 60 * 1000,
+        secret: 'SuperGeheim'
+    }))
 
 // configure the templating system
 nunjucks.configure('views', {
@@ -66,7 +94,7 @@ nunjucks.configure('views', {
 })
 
 // Default landing page with list of linked patients
-app.get('/', getRequestAccessToken, (req, res, next) => {
+app.get('/', loginFilter(loginUrl), getRequestAccessToken, (req, res, next) => {
     fetchPatientList(req.params.access_token)
         .then((result) => {
             res.render('home.html', { bundle: result })
@@ -78,7 +106,7 @@ app.get('/', getRequestAccessToken, (req, res, next) => {
 })
 
 // Fetch the patients data and render in a template
-app.get('/patient/:id', getRequestAccessToken, (req, res, next) => {
+app.get('/patient/:id', loginFilter(loginUrl), getRequestAccessToken, (req, res, next) => {
     fetchPatient(req.params.id, req.params.access_token)
         .then((results) => {
             res.render('patient.html', {
@@ -93,10 +121,43 @@ app.get('/patient/:id', getRequestAccessToken, (req, res, next) => {
 })
 
 // Uses the connect api to add another patient to the application
-app.get('/add_patient', getRequestAccessToken, (req, res, next) => {
-    const redirectUrl = `https://api.1up.health/connect/system/clinical/${health_system_id}?client_id=${req.params.client_id}&access_token=${req.params.access_token}`
+app.get('/add_patient', loginFilter(loginUrl), getRequestAccessToken, (req, res, next) => {
+    const redirectUrl = `https://api.1up.health/connect/system/clinical/${healthSystemId}?client_id=${req.params.client_id}&access_token=${req.params.access_token}`
     logger.info('Redirecting to %s', redirectUrl)
     res.redirect(302, redirectUrl)
+})
+
+// login page
+app.get('/login', (req, res, next) => {
+    const targetPage = req.query.target || '/'
+    if (req.session.userIsLoggedIn) {
+        logger.debug('User already logged in, redirecting to %s', targetPage)
+        res.redirect(302, targetPage)
+    } else {
+        res.render('login.html',
+            {
+                target: targetPage,
+                error: (req.query.error || null)
+            }
+        )
+    }
+})
+
+app.post('/login', (req, res, next) => {
+    const targetPage = req.body.target || '/'
+    if (req.session.userIsLoggedIn) {
+        res.redirect(302, targetPage)
+    } else {
+        if (req.body.password === appPassword) {
+            logger.info('Successful login. Redirecting to target %s', targetPage)
+            req.session.userIsLoggedIn = true
+            res.redirect(302, targetPage)
+        } else {
+            const loginRedirect = loginUrl + '?' + querystring.stringify({ target: targetPage, error: 'Invalid Access Code' })
+            logger.info('Invalid login. Redirecting to %s', loginRedirect)
+            res.redirect(302, loginRedirect)
+        }
+    }
 })
 
 app.listen(port, () => {
